84947dbabfef6c554a6f0620d07ef386afe51776
masterBaster <bartek.baster@gmail.com>
2018-08-15 18:42:50 +0200

    working with function expression

--- app_2.js
+++ app_2.js
@@ -1,2 +1,19 @@
 console.log(__dirname);
 console.log(__filename);
+
+function say_hello(){
+    console.log('hello world');
+}
+say_hello();
+
+//function expression
+
+let say_hi = function(){
+    console.log('say hi');
+}
+say_hi();
+
+let say_bye = function(parameter){
+    console.log(parameter);
+}
+say_bye('say good bye');

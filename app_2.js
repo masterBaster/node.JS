cff4796659c77a847d47e1fa0f075597d9ccd5e2
masterBaster <bartek.baster@gmail.com>
2018-08-17 17:54:33 +0200

    cut useful code

--- app_2.js
+++ app_2.js
@@ -27,7 +27,7 @@ let say_bye = function(parameter){
 say_bye('say good bye');
 
 const counter = require('./app_3.js');
-console.log(counter(['yell', 'crystal']));
+console.log(counter.counter(['yell', 'crystal']));
 
 const counter_pattern = require('./app_3.js')
 console.log(counter_pattern.counter_pattern(['yell', 'crystal', 'yoshi']));
@@ -38,11 +38,15 @@ console.log(adder.adder(1,2,3));
 const cube = require('./app_3.js');
 console.log(cube.cube(1,5,6));
 
+//working with particular object using differnt types of method
+
+const mathematic = require('./app_3.js');
+console.log(mathematic.counter_pattern(['youshi', 'moushi', 'yele']));
+
 const Square = require('./app_4.js');
 const mySquare = new Square(5);
 console.log(`the area of mySquare is ${mySquare.area()}`);
+console.log(`the cube of these three numbers equal ${mySquare.cuboid()}`);
 
-const Square_2 = require('./app_4.js');
-const mySquare_2 = new Square_2(3);
-console.log(`the value of cube is ${mySquare_2.cuboid()}`);
+
 
--- app_3.js
+++ app_3.js
@@ -1,22 +1,17 @@
-var counter = function(arr_param){
+module.exports.counter = function(arr_param){
     return 'there are ' + arr_param.length + ' elements in this array';
 }
 
-module.exports = counter;
-
-var counter_pattern = function(arr_param){
+module.exports.counter_pattern = function(arr_param){
     return 'there are ' + arr_param.length + 'elements in this array (inside counter_pattern)';
 }
 
-var cube = function(a,b,h){
+module.exports.adder = function(a,b,h){
     return `the cube of these three numbers is equal ${a*b*h}`;
 };
 
-var adder =  function(a,b,c){
+module.exports.cube =  function(a,b,c){
     return `the sume of three numbers is ${a+b+c}`;
 }
 
-module.exports.counter_pattern = counter_pattern;
-module.exports.adder = adder;
-module.exports.cube = cube;
 

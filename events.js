42eb259eb40a0c279fba1c943e66923639182609
masterBaster <bartek.baster@gmail.com>
2018-08-18 09:25:07 +0200

    attached custom events to each bbplayers

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const util = require('util');
 
 class MyEmitter extends EventEmitter {
   constructor() {
@@ -10,4 +11,16 @@ const myEmitter = new MyEmitter();
 myEmitter.on('some_event', function(message) {
   console.log(message);
 });
-myEmitter.emit('some_event', 'fire some_event');
+myEmitter.emit('some_event', 'message: fire some_event');
+
+
+const BasketBallPlayers = function(name, surname, age){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+}
+
+util.inherits(BasketBallPlayers, EventEmitter.EventEmitter);
+const james = new Person(James, Harden, 30);
+const chris = new Person(Chris, Paul, 29);
+const kevin = new Person(Kervin, Durant, 30);

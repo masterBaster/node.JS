7768e5d21eef0125c6bfb807f95eeed567f3cc68
masterBaster <bartek.baster@gmail.com>
2018-08-19 16:46:24 +0200

    server ran at port 3000

--- http.js
+++ http.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
-const server = http.createServer(function(req, res){
+const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('everything is okay');
 });
 
-server.listen(3000, '127.0.0.1')
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+    console.log(`server running at port ${port}`);
+})

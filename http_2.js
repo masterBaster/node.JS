61f7c32b91406f36f6888ce64d4350f464b3cdd0
masterBaster <bartek.baster@gmail.com>
2018-08-22 11:36:16 +0200

    added pipe method

--- http_2.js
+++ http_2.js
@@ -1,23 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
     console.log('request was made: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
         const MyReadStream = fs.createReadStream(__dirname + '/readMe.txt');
-        const MyWriteStream = fs.createWriteStream(__dirname + '/writeStream.txt');
-            MyReadStream.pipe(MyWriteStream);
+            MyReadStream.pipe(res);
                 setTimeout(() => {
                         console.log('stop writing to writeStream.txt');
                     MyReadStream.unpipe(MyWriteStream);
                         console.log('manually close the file stream')
                     MyWriteStream.end();
-                }, 1000);
-            res.end('everything is okay');
+                }, 20000);
 });
-
-const hostname = '127.0.0.1';
-const port = 3000;
 
 server.listen(port, hostname, () => {
     console.log(`server running at port ${port}`);
@@ -26,8 +24,10 @@ server.listen(port, hostname, () => {
 
 
 /*
-    MyReadStream.on('data', (chunk) => {
-    console.log('new chunk received...');
-    MyWriteStream.write(chunk);
+    const MyReadStream = fs.createReadStream(__dirname + '/readMe.txt');
+    const MyWriteStream = fs.createWriteStream(__dirname + '/writeStream.txt');
+        MyReadStream.on('data', (chunk) => {
+            console.log('new chunk received...');
+        MyWriteStream.write(chunk);
 }); 
 */

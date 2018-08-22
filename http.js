f40c92dbed995b88505b48bab5c8af7afeef3337
masterBaster <bartek.baster@gmail.com>
2018-08-22 11:24:43 +0200

    send some date to client

--- http.js
+++ http.js
@@ -12,23 +12,3 @@ MyReadStream.on('end', () => {
     console.log('there are no more data.');
 });
 
-
-
-
-
-
-
-/*
-const server = http.createServer((req, res) => {
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('everything is okay');
-});
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-server.listen(port, hostname, () => {
-    console.log(`server running at port ${port}`);
-})
-*/
--- http_2.js
+++ http_2.js
@@ -1,9 +1,33 @@
 const http = require('http');
 const fs = require('fs');
 
-const MyReadStream = fs.createReadStream(__dirname + '/readMe.txt');
-const MyWriteStream = fs.createWriteStream(__dirname = '/writeStream.txt');
-
-MyReadStream.on('readable', () => {
-    console.log(`readable: ${MyReadStream.read()}`);
+const server = http.createServer((req, res) => {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+        const MyReadStream = fs.createReadStream(__dirname + '/readMe.txt');
+        const MyWriteStream = fs.createWriteStream(__dirname + '/writeStream.txt');
+            MyReadStream.pipe(MyWriteStream);
+                setTimeout(() => {
+                        console.log('stop writing to writeStream.txt');
+                    MyReadStream.unpipe(MyWriteStream);
+                        console.log('manually close the file stream')
+                    MyWriteStream.end();
+                }, 1000);
+            res.end('everything is okay');
 });
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+    console.log(`server running at port ${port}`);
+});
+
+
+
+/*
+    MyReadStream.on('data', (chunk) => {
+    console.log('new chunk received...');
+    MyWriteStream.write(chunk);
+}); 
+*/

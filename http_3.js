bdbd4cad9bb2ee5891e9a4be7fd75ea5e6237e92
masterBaster <bartek.baster@gmail.com>
2018-08-23 10:17:30 +0200

    serving json data

--- http_3.js
+++ http_3.js
@@ -6,9 +6,14 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-        const MyReadStream = fs.createReadStream(__dirname + '/index.html');
-            MyReadStream.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    
+    const MyObject = {
+        name: 'James',
+        surname: 'Harden',
+        age: 28
+    };
+    res.end(JSON.stringify(MyObject));
 });
 
 server.listen(port, hostname, () => {

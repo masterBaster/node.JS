5db5acd940452d9289e6df0a9910748fe69a78dc
masterBaster <bartek.baster@gmail.com>
2018-08-23 10:55:50 +0200

    working with server req

--- http_3.js
+++ http_3.js
@@ -9,7 +9,18 @@ const server = http.createServer((req, res) => {
     if(req.url === '/home' || req.url === '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }
+    } else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/players'){
+        const players = [
+            {name: 'James', age: 28}, 
+            {name: 'Tom', age: 24}
+    ]
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(players));
+};
+
 });
 
 server.listen(port, hostname, () => {

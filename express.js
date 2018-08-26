d9634774555882302ecaa78d4cc155be2355d8a7
masterBaster <bartek.baster@gmail.com>
2018-08-26 22:29:04 +0200

    injected data interviews

--- express.js
+++ express.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     res.send('welcome to my homepage');
 });
 
 app.get('/contact', function(req, res){
-    res.send('welcome to my homepage');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/homepage', function(req, res){
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/profile/:id', function(req, res){
-    res.send('You requested to see a profile with the id of:' + req.params.id);
+app.get('/profile/:name', function(req, res){
+    let data = { age: 29, job: 'progremmmer'};
+    res.render('profile', {person: req.params.name, data: data});  
 })
 
 app.listen(3000);

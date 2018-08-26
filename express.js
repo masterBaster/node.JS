77364497d2ee00d914021393751d3bde7cab2ec5
masterBaster <bartek.baster@gmail.com>
2018-08-26 21:01:24 +0200

    developing template engines for Express

--- express.js
+++ express.js
@@ -9,5 +9,13 @@ app.get('/contact', function(req, res){
     res.send('welcome to my homepage');
 });
 
+app.get('/homepage', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/profile/:id', function(req, res){
+    res.send('You requested to see a profile with the id of:' + req.params.id);
+})
+
 app.listen(3000);
 

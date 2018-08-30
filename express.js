4c89597b1f92f6b1b787b273d8d7665894bae5c0
masterBaster <bartek.baster@gmail.com>
2018-08-30 22:40:28 +0200

    used POST method

--- express.js
+++ express.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 app.get('/contact', function(req, res){
+    res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body)
     res.render('contact', {qs: req.query});
 });
 

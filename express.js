37f60332728e7e276c58b829a19ed6f9070be6bc
masterBaster <bartek.baster@gmail.com>
2018-08-30 22:13:14 +0200

    added contact form

--- express.js
+++ express.js
@@ -5,8 +5,7 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
 app.get('/contact', function(req, res){
-    console.log(req.query)
-    res.render('contact');
+    res.render('contact', {qs: req.query});
 });
 
 app.get('/homepage', function(req, res){
